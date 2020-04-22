a790d0b8da3e4c1ab31f287d6217d0830375106f
blau <>
2020-04-22 23:24:41 +0900

    add: ユーザー編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,26 @@ class UsersController < ApplicationController
       render("users/new")
     end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/users/index")
+    else
+      flash[:alert] = "不正な入力です"
+      render("users/edit")
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'users/index' => "users#index"
   get 'users/new' => "users#new"
   post 'users/create' => "users#create"
+  get 'users/:id' => "users#show"
+  get 'users/:id/edit' => "users#edit"
+  post 'users/:id/update' => "users#update"
 
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
