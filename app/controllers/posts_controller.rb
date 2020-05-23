08fb6b4cb32411574c49a8f9d9b6993f84622839
blau <le.space@icloud.com>
2020-05-23 16:52:34 +0900

    add user-posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,10 +15,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id)
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 100}}
+    validates :user_id, {presence: true}
 
     def self.search(searchword)
         return Post.where(['content LIKE ?', "%#{searchword}%"])
