2e134e60bcb16038ffb7d92548ee08445d0eb4d0
blau <le.space@icloud.com>
2020-05-23 17:00:21 +0900

    refactor

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 100}}
     validates :user_id, {presence: true}
 
+    def user
+        return User.find_by(id: self.user_id)
+    end
+
     def self.search(searchword)
         return Post.where(['content LIKE ?', "%#{searchword}%"])
     end
