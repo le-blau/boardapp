cccab2f7ec6baa943759f095f6393511864a2ce8
blau <le.space@icloud.com>
2020-05-25 00:50:51 +0900

    add DB seed value

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# def numbering_user
+#   return User.maximum(:id) + 1
+# end
+
+
+# test ユーザー 1人
+User.create(
+  id: "0",
+  name: "test",
+  email: "test@example.com",
+  password: "test"
+)
+fruits = ["apple", "orange", "melon", "banana", "pineapple", "strawberry", "acerola", "cherry", "kiwi", "lemon"]
+10.times do |i|
+  Post.create(
+    content: fruits.sample(rand(10)).join(' '),
+    user_id: "0"
+  )
+end
+
+# seed ユーザー10人
+10.times do |i|
+  User.create(
+    name: "seed_user#{i + 1}",
+    email: "seed#{i + 1}@example.com",
+    password: "seed#{i + 1}"
+  )
+  Post.create(
+    content: "seeeeeeeeeeeeeeeeeeeeeeeeeed_content#{i + 1}",
+    user_id: "#{User.maximum(:id)}" # Post.user_idには、直前のUser.create()で作ったUser.idを設定する
+  )
+end
